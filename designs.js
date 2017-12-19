4b2b9867428f60d2d046be8ba1e55d2be2dd66a5
Amr Adel <32145262+amr-adel@users.noreply.github.com>
2017-12-19 21:21:41 +0200

    Basic functions, pure JS

--- designs.js
+++ designs.js
@@ -1,10 +1,46 @@
-// Select color input
-// Select size input
+// Setting consts
+const picker = document.getElementById('colorPicker'); // For color picker
 
-// When size is submitted by the user, call makeGrid()
+const grid = document.getElementById('pixel_canvas'); // For table
+const start = document.getElementById('start'); // For submit botton
+
+const gridHeight = document.getElementById('input_height'); // For number of rows
+const gridWidth = document.getElementById('input_width'); // For number of columns
+
 
 function makeGrid() {
 
-// Your code goes here!
+	for (var i = 0; i < gridHeight.value; i++) {
 
-}
+		// Add single row
+		var row = grid.insertRow();
+
+		for (var x = 0; x < gridWidth.value; x++) {
+			// Add single cell
+			row.insertCell()
+		};
+	};
+
+	grid.addEventListener('click', function (event) {
+		if (event.target.tagName === "TD") {
+			event.target.style.backgroundColor = picker.value;
+		}
+	});
+
+};
+
+
+// To do on submit button click
+start.addEventListener('click', function (event) {
+
+	// Prevent submitting form
+	event.preventDefault();
+
+
+	// Reset table
+	grid.innerHTML = '';
+
+	// Create Grid
+	makeGrid()
+
+});
