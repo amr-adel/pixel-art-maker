6bc9d47ed26d916a0c42d440fb36c560dca9e33a
Amr Adel <32145262+amr-adel@users.noreply.github.com>
2017-12-27 02:05:59 +0200

    Set initial values according to viewport
    Calculate optimal number of rows and columns according to viewport dimensions

--- designs.js
+++ designs.js
@@ -1,11 +1,18 @@
 // Setting consts
-const picker = document.getElementById('colorPicker'); // For color picker
-
-const grid = document.getElementById('pixel_canvas'); // For table
 const start = document.getElementById('start'); // For submit botton
+const grid = document.getElementById('pixel_canvas'); // For table
+
+const picker = document.getElementById('colorPicker'); // For color picker
 
 const gridHeight = document.getElementById('input_height'); // For number of rows
 const gridWidth = document.getElementById('input_width'); // For number of columns
+
+const heightValue = Math.floor((window.innerHeight - 90) / 20); // Calculate optimal number of rows according to viewport height
+const widthValue = Math.floor((window.innerWidth - 40) / 20); // Calculate optimal number of columns according to viewport width
+
+
+gridHeight.setAttribute("value", heightValue); // Set initial grid height value
+gridWidth.setAttribute("value", widthValue); // Set initial grid width value
 
 
 function makeGrid() { // Build canvas (table)
