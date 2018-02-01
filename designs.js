38415c7798d53f46e6bc61a02ac79d579b32802e
Amr Adel <32145262+amr-adel@users.noreply.github.com>
2018-02-01 04:06:02 +0200

    Add erase function

--- designs.js
+++ designs.js
@@ -1,36 +1,181 @@
-const gridHeight = document.getElementById('input-height'); // For number of rows
-const gridWidth = document.getElementById('input-width'); // For number of columns
+(function() {
+	const gridHeight = document.getElementById('input-height'); // For number of rows
+	const gridWidth = document.getElementById('input-width'); // For number of columns
 
-const heightValue = Math.floor((window.innerHeight - 90) / 20); // Calculate optimal number of rows according to viewport height
-const widthValue = Math.floor((window.innerWidth - 40) / 20); // Calculate optimal number of columns according to viewport width
+	const heightValue = Math.floor((window.innerHeight - 240) / 20); // Calculate optimal number of rows according to viewport height
+	const widthValue = Math.floor((window.innerWidth - 40) / 20); // Calculate optimal number of columns according to viewport width
 
 
-gridHeight.setAttribute("value", heightValue); // Set initial grid height value
-gridWidth.setAttribute("value", widthValue); // Set initial grid width value
+	gridHeight.setAttribute("value", heightValue); // Set initial grid height value
+	gridWidth.setAttribute("value", widthValue); // Set initial grid width value
+	
+	const main = document.getElementById('container');
+	const grid = document.getElementById('canvas-grid'); // For table
+	const start = document.getElementById('start'); // For submit botton
+	
+	const reset = document.getElementById('reset');	
+
+	
+	const picker = document.getElementById('picker');
+	const pickerPh = document.getElementById('picker-ph');
+	
+
+	pickerPh.addEventListener('click', function() {
+		picker.focus();
+		picker.click();
+	})
+
+	picker.addEventListener('change', function() {
+		pickerPh.style.backgroundColor = picker.value;
+		
+		for (let i = 0; i < recent.length; i++) {
+			document.getElementById('recent' + i).style.backgroundColor = recent[i];
+		}
+	})
+
+	
+	
+	
+	
+	let active = false; // Paint **************************************************************************************
+
+	const paint = function (cell) {
+		if (active) {
+			if (cell.target.tagName === "TD") {
+				cell.target.style.backgroundColor = picker.value;
+			};
+		}
+	}
+	
+	const erase = function (cell) {
+		if (active) {
+			if (cell.target.tagName === "TD") {
+				cell.target.style.backgroundColor = "";
+			};
+		}
+	}
+
+	grid.addEventListener('mousedown', function (e) {
+		active = true;
+		let tool = document.querySelector('input[name="tool"]:checked').value;
+		
+		if (tool === "paint") {
+			paint(e);
+			addRecent();
+		} else if (tool === "erase") {
+			erase(e);
+		}
+	})
+
+	grid.addEventListener('mouseleave', function () {
+		active = false;
+	})
+
+	document.addEventListener('mouseup', function () {
+		active = false;
+	})
+
+	grid.addEventListener('mouseover', function(e) {
+		
+		let tool = document.querySelector('input[name="tool"]:checked').value;
+		
+		if (tool === "paint") {
+			paint(e);
+		} else if (tool === "erase") {
+			erase(e);
+		}
+	});
+	
+	
+	
+	
+	
+	const recent = []; // Add recent color ***********************************************************************************
+	
+	
+	function addRecent() {
+
+		if (recent.length == 0) {
+			recent.unshift(picker.value);
+		} else {
+			for (let i = 0; i < recent.length; i++) {
+				if (recent[i] === picker.value) {
+					recent.splice(i, 1);
+				}
+			}
+			if (recent.length < 5) {
+				recent.unshift(picker.value)
+			} else if (recent.length >= 5) {
+				recent.pop();
+				recent.unshift(picker.value);
+			}
+		}
+
+	}
+	
+	
+	function makeGrid() { // Build canvas (table) ****************************************************************************************
+
+		for (let i = 0; i < gridHeight.value; i++) {
+
+			// Add single row
+			let row = grid.insertRow();
+
+			for (let x = 0; x < gridWidth.value; x++) {
+				// Add single cell
+				row.insertCell()
+			};
+		};
+
+	};
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	start.addEventListener('click', function (event) { // To do on submit button click ****************************************************************************************
+
+		// Prevent submitting form
+		event.preventDefault();
+
+
+		// Reset table
+		grid.innerHTML = '';
+
+		main.classList.add('canvas');
+
+		// Create Grid
+		makeGrid()
+
+	});
+
+
+	reset.addEventListener('click', function () { // Reset Canvas  ****************************************************************************************
+		main.classList.remove('canvas');
+	})
+
+}());
+
+
 /*
 
 // Setting consts
-const start = document.getElementById('start'); // For submit botton
-const grid = document.getElementById('pixel_canvas'); // For table
+
 
 const picker = document.getElementById('colorPicker'); // For color picker
 
 
 
-function makeGrid() { // Build canvas (table)
-
-	for (let i = 0; i < gridHeight.value; i++) {
-
-		// Add single row
-		let row = grid.insertRow();
-
-		for (let x = 0; x < gridWidth.value; x++) {
-			// Add single cell
-			row.insertCell()
-		};
-	};
-
-};
 
 
 (function () { // Change cell background-color when clicked or hover over with mouse button down
@@ -91,39 +236,11 @@ function addRecent() {
 }
 
 
-start.addEventListener('click', function (event) { // To do on submit button click
-
-	// Prevent submitting form
-	event.preventDefault();
-
-
-	// Reset table
-	grid.innerHTML = '';
-
-	// Create Grid
-	makeGrid()
-
-});
-
-
-*/
-
-
-const main = document.getElementById('container');
 
 
 
-const picker = document.getElementById('picker');
-const pickerPh = document.getElementById('picker-ph');
 
-pickerPh.addEventListener('click', function() {
-	picker.focus();
-	picker.click();
-})
 
-picker.addEventListener('change', function() {
-	pickerPh.style.backgroundColor = picker.value;
-})
 
 //=======================================================================
 
@@ -148,14 +265,8 @@ bgPicker.addEventListener('change', function() {
 
 //=======================================================================
 
-const reset = document.getElementById('reset');
-const start = document.getElementById('start');
 
-start.addEventListener('click', function (event) {
-	event.preventDefault();
-	main.classList.add('canvas');
-})
 
-reset.addEventListener('click', function () {
-	main.classList.remove('canvas');
-})
+
+
+*/
